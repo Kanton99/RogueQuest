a41973cfa700de613d72972ee79ac648dc5600c7
Kanton99 <avolkov99@gmail.com>
2025-04-16 16:17:03 +0200

    moved full level generation to scene reset

--- Assets/_RogueQuest/Scripts/Procedural/Scene Creation/SceneInstantiation.cs
+++ Assets/_RogueQuest/Scripts/Procedural/Scene Creation/SceneInstantiation.cs
@@ -2,13 +2,14 @@
 using UnityEngine.SceneManagement;
 using UnityEditor.SceneTemplate;
 using System;
+using System.Collections;
+using UnityEditor;
+using UnityEditor.SceneManagement;
 
 public class SceneInstantiation : MonoBehaviour
 {
 	public SceneInstantiation Instance { get; private set; }
-	public SceneTemplateAsset sceneTemplate;
-
-	private Awaitable<string> newLevel;
+	public LevelGenerator levelGenerator;
 
 	private void Awake(){
 		if (Instance != null && Instance != this)
@@ -22,48 +23,24 @@ public class SceneInstantiation : MonoBehaviour
 		}
 	}
 
+	public void NewLevelSteps()
+	{
+		//Fade to black
+		//Place player at the start of the level
+		GenerateLevel();	
+	}
+
 	[ContextMenu("Generate Next Level")]
 	public void GenerateLevel()
 	{
-		if(sceneTemplate == null)
+		if(levelGenerator == null)
 		{
-			Debug.LogError("No scene template assigned.");
+			Debug.LogError("No level generator assigned.");
 			return;
 		}
 		int seed = UnityEngine.Random.Range(0, int.MaxValue);
-		newLevel = GenerateScene(seed);
-	}
 
-	[ContextMenu("Load Next Level")]
-	public async void LoadLevel(){
-		await newLevel;
-	}
-
-	private async Awaitable<string> GenerateScene(int seed)
-	{
-		await Awaitable.BackgroundThreadAsync();
-		string levelPath = "Assets/Scenes/Levels/"+seed;
-
-		if (sceneTemplate != null)
-		{
-			try
-			{
-				LevelTemplatePipeline.seed = seed;
-				UnityEditor.SceneTemplate.SceneTemplateService.Instantiate(sceneTemplate, true);
-				Debug.Log("Scene template instantiated successfully.");
-				return levelPath;
-			}
-			catch(Exception e)
-			{
-				Debug.LogError("Failed to instantiate scene template: "+e.Message);
-				return null;
-			}
-		}
-		else
-		{
-			Debug.LogError("No scene template assigned.");
-			return null;
-		}
-
+		levelGenerator.seed = (uint)seed;
+		levelGenerator.GenerateLevelSteps();
 	}
 }
